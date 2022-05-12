a14686ffd48c75a6cb551dc0245736a004758510
Angell Belger <angellbelger@hotmail.com>
2022-05-12 18:31:37 -0300

    fineshed AE01

---
+++ Data structure/AE01.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *p;
+
+    printf("\nHello, world.\n\n");
+
+    p = (int *) malloc(sizeof(int));
+    if (p == NULL){
+        printf("\nError\n");
+    }else {
+        *p = 54;
+        printf("Pointer adress: %p\n", p);
+        printf("The value = %i\n", *p);
+    }
+    return 0;
+}
